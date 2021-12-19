61b3156b8098057b4254894fc1a05495854d64a9
pomonella <39027165+pomonella01@users.noreply.github.com>
2021-12-19 12:33:13 -0500

    we have system info now

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// No, I won't be using the new C# templates for dotnet 6. C# is not a scripting language.
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Loom // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args) // A classic.
+        {
+            Console.WriteLine(@"Welcome to Loom!");
+            var OS = new Loom.Sys();
+            OS.DetectParams();
+        } 
+    }
+}
