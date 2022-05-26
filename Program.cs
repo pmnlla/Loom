1b1fdbc1c7c8c712bfa66cd054ccf60d4d692908
pomonella01 <39027165+pomonella01@users.noreply.github.com>
2022-05-25 20:44:10 -0400

    it's working... somehow.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using Checkbox;
+using Phosphorus;
+
 namespace Loom // Note: actual namespace depends on the project name.
 {
     public class Program
@@ -14,44 +15,10 @@ namespace Loom // Note: actual namespace depends on the project name.
         public static void Main(string[] args) // A classic.
         {
             // start tthis shit
-            Console.WriteLine("Welcome to Loom!");
+            Console.WriteLine("done");
             
-            Checklist();
-        } 
-
-        // this does the echecklist stuff
-        public static void Checklist(){
-            // TODO // pass OS to this function instead of creating a new one for garbage collection purposes
-            var OS = new Loom.Sys();
-            OS.DetectParams();
-            OS.PopulateArray();
-
-            string title = "Please select the mods you would like to install. \nPlease do note that mods found on CurseForge and not Modrinth are unavailable due to the way that Curseforge's CDN works. \nNONE OF THE MODS INSTALL AUTOMATICALLY AT THE MOMENT.";
-            // for (int i = 0; i > OS.modlist.Count; i++){
-            int i = 0;
-            foreach (modProperties mod in OS.modlist){
-                try{
-                    opts[i] = mod.Name;
-                }
-                catch (Exception e){
-                    Array.Resize(ref opts, opts.Length + 1);
-                    opts[i] = mod.Name;
-                }
-                i++;
-            }
-            var cbox = new Checkbox.Checkbox(title, true, false, opts);
-            var output = cbox.Select();
-
-            foreach(var returnValue in output){
-                OS.modlist[returnValue.Index].Use = true;
-            }
-            // debugging
-            foreach (modProperties mod in OS.modlist){
-                if (mod.Use == true){
-                    OS.dlmod(mod.Url, (OS.dir.ToString() + "/mods/" + mod.Name + ".jar"));
-                }
-            }
-            
+            mainWindow win = new mainWindow();
+            win.init();
         }
     }
 }
--- ui/drawMainWindow.cs
+++ ui/drawMainWindow.cs
@@ -3,11 +3,31 @@ using Terminal.Gui;
 
 namespace Phosphorus{
 
-    class mainWindow{
+    public partial class mainWindow{
 
-        public static void init() {
+        public void init() {
 
+            Application.Init();
+            setupCheckboxes(Application.Top);
+            var menu = new MenuBar (new MenuBarItem [] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_Quit", "", () => { 
+                        Application.RequestStop (); 
+                    })
+                }),
+            });
+            Application.Top.Add (menu);
+            setupCheckboxes(Application.Top);
+            Application.Run();
+            Application.Shutdown();
+
+        }
+        public async void setupCheckboxes(View selectionView) {
             
+            for (int i = 0; i < 5; i++){
+                var checkbox = new CheckBox (0, i+1, "test");
+                selectionView.Add(checkbox);
+            }
 
         }
     }
