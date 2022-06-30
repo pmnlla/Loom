e7c1d93c17095e23f9ee3bdda9e7c1d5dff2d6f4
pomonella01 <39027165+pomonella01@users.noreply.github.com>
2022-06-30 12:00:37 -0400

    i'm a girl, i'm a ghost, i'm a gameshow

---
+++ sys/modrinthInterface/main.cs
@@ -0,0 +1,14 @@
+using System;
+using Phosphorus;
+
+namespace Phosphorus{
+    public partial class ModrinthInterface {
+        // todo: get this shit done.
+
+        /// <summary>
+        /// This is how we will interface with the modrinth API.
+        /// We will use this to get the latest versions and download links of mods, along with other metadata.
+        /// Eventually, we'll be able to merge download.cs into this, and params.cs will become a different thing entirely.
+        /// </summary>
+    }
+}
